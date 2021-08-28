98376cb03aeff2ed6dfef7e1c73d9f53b5964bf4
xiaoxigua <aijdfajodwsdf@gmail.com>
2021-08-28 22:36:18 +0800

    navbar mobile device sass

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import cornhub from './img/cornhub.png';
 import './sass/navbar.sass';
 
 const Navbar = () => {
+  const [check, setCheck]= useState(false);
+  const checkFun = () => {
+    console.log(check);
+    setCheck(!check);
+    document.getElementById('check').checked = check;
+  };
+
   return (
-    <ul id="navbar">
+    <ul id="navbar" style={{height: check ? '100px' : ''}}>
       <li id="logo">
         <img src={cornhub} />
       </li>
-      <li>
+      <li id="navbar-button">
+        <input id="check" type="checkbox" onClick={checkFun}/>
+        <div></div>
+      </li>
+      <li className="text">
         影片
       </li>
-      <li>
+      <li className="text">
         分類
       </li>
-      <li>
+      <li className="text">
         照片與動圖
       </li>
     </ul>
