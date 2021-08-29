0e28704801b2611d9c32b6887e1ff730884872cf
xiaoxigua <aijdfajodwsdf@gmail.com>
2021-08-29 13:16:12 +0800

    HashRouter !!!

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import NavBar from './components/navbar';
 import Home from './components/home';
 import Video from './components/video';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Switch,
   Route,
 } from 'react-router-dom';
